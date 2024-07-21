b0f7b3f77488c9ec785dda968cd184025fcb618e
Travelquides <nffn7766@gmail.com>
2024-07-21 12:37:59 +0800

    Update script.js

--- web/script.js
+++ web/script.js
@@ -1,10 +1,3 @@
-<<<<<<< HEAD
 document.addEventListener("DOMContentLoaded", function() {
     // Add any JavaScript code here
-  });
-=======
-document.addEventListener("DOMContentLoaded", function() {
-    // Add any JavaScript code here
-  });
->>>>>>> 2e4565917370617f526e49ea4f202e84e78cbf50
-  
+});
